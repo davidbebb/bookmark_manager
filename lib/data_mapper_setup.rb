49c14a3448754e2d1ef7b10aecfd27e9858430fd
davidbebb <davidbebb@gmail.com>
2015-09-22 16:40:19 +0100

    fixed database url back from url

--- lib/data_mapper_setup.rb
+++ lib/data_mapper_setup.rb
@@ -2,8 +2,8 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 env = ENV['RACK_ENV'] || 'development'
-DataMapper.setup(:default, "postgres://ohbshfahaaymsg:WdR8nuKGUXpmMQRsouqe2MDjXC@ec2-54-204-25-54.compute-1.amazonaws.com:5432/dbjdh8pcmtb2se")
-#DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+#DataMapper.setup(:default, "postgres://ohbshfahaaymsg:WdR8nuKGUXpmMQRsouqe2MDjXC@ec2-54-204-25-54.compute-1.amazonaws.com:5432/dbjdh8pcmtb2se")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 DataMapper::Logger.new($stdout, :debug)
 
 require './app/models/link'
